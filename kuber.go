9eca1e4ae8b2fcc7a6925247204b1fa7143ec86e
Bruno Moiteiro <bmoiteiro@recordtv.com.br>
2019-01-21 09:33:02 -0200

    Adding service definition

--- kuber.go
+++ kuber.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 )
 
 func main() {
+	log.Println("Starting Server")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "Hello there")
+		log.Println("Processing request")
+		io.WriteString(w, "Hello there, updating content without destroying service")
 	})
 
 	http.ListenAndServe("0.0.0.0:8080", nil)
